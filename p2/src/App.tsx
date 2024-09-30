075941d30a28c5ae426cc1b940e5e5a793cb59c3
mohamedmaher <mm6857639@gmail.com>
2024-09-30 11:02:00 +0300

    Enhance button, input and form

--- p2/src/App.tsx
+++ p2/src/App.tsx
@@ -5,6 +5,8 @@ import ProductCard from './coponent/Product'
 import {productList} from "./data"
 import { useState } from 'react'
 import Button from './coponent/Ui/Button'
+import { formInputsList } from './data'
+import Input from './coponent/Ui/Input'
 
 function App() {
     const [isOpen, setIsOpen] = useState(false)
@@ -17,6 +19,12 @@ function App() {
     setIsOpen(false)
   }
   const renderList = productList.map(product => <ProductCard key={product.id} product={product} />)
+  const renderFormList = formInputsList.map(input => (
+    <div className='flex flex-col'>
+      <label className='mb-[1px] text-sm font-medium text-gray-700' htmlFor={input.id}>{input.label}</label>
+      <Input type="text" id={input.id} name={input.name} />
+    </div>
+  ))
   return (
     <>
     <main className='container'>
@@ -24,10 +32,15 @@ function App() {
     <div className='m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md'>
     {renderList}
     <Modal  isOpen={isOpen} close={close} title='ADD A NEW PRODUCT'>
-<div className='flex items-center space-x-3'>
+      <form className='space-y-2'>
+      {renderFormList}
+      <div className='flex items-center space-x-3'>
 <Button className=" bg-indigo-600 hover:bg-indigo-800">SUPMIT</Button>
     <Button className=" bg-red-600 hover:bg-red-800">CANSEL</Button>
 </div>
+
+      </form>
+      
 
     </Modal>
     
--- p2/src/coponent/Ui/Modal.tsx
+++ p2/src/coponent/Ui/Modal.tsx
@@ -18,7 +18,7 @@ const modal = ({isOpen, close, title, children}: Iprops) =>{
           <div className=" flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              className="w-full max-w-md p-sm shadow-sm rounded-xl bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
             >
               
               {title && <DialogTitle as="h3" className="text-base/7 font-medium text-black">
